e43c6706cd3044ee75590e73f8a92d9bea70d754
sohnryang <loop.infinitely@gmail.com>
2023-04-23 22:11:12 +0900

    fix(lab4): use `lab4_2` module as specs

--- lab4/lab4/lab4.sim/sim_1/behav/xsim/xsim.dir/lab4_3_tb_behav/obj/xsim_1.c
+++ lab4/lab4/lab4.sim/sim_1/behav/xsim/xsim.dir/lab4_3_tb_behav/obj/xsim_1.c
@@ -54,39 +54,35 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_18(char*, char *);
-IKI_DLLESPEC extern void execute_56(char*, char *);
-IKI_DLLESPEC extern void execute_57(char*, char *);
-IKI_DLLESPEC extern void execute_58(char*, char *);
-IKI_DLLESPEC extern void vlog_const_rhs_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
-IKI_DLLESPEC extern void execute_60(char*, char *);
-IKI_DLLESPEC extern void execute_61(char*, char *);
-IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
-IKI_DLLESPEC extern void execute_51(char*, char *);
-IKI_DLLESPEC extern void execute_52(char*, char *);
+IKI_DLLESPEC extern void execute_19(char*, char *);
 IKI_DLLESPEC extern void execute_53(char*, char *);
 IKI_DLLESPEC extern void execute_54(char*, char *);
 IKI_DLLESPEC extern void execute_55(char*, char *);
-IKI_DLLESPEC extern void execute_29(char*, char *);
-IKI_DLLESPEC extern void execute_25(char*, char *);
+IKI_DLLESPEC extern void vlog_const_rhs_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+IKI_DLLESPEC extern void execute_57(char*, char *);
+IKI_DLLESPEC extern void execute_58(char*, char *);
+IKI_DLLESPEC extern void execute_52(char*, char *);
+IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+IKI_DLLESPEC extern void execute_30(char*, char *);
 IKI_DLLESPEC extern void execute_26(char*, char *);
-IKI_DLLESPEC extern void execute_20(char*, char *);
+IKI_DLLESPEC extern void execute_27(char*, char *);
 IKI_DLLESPEC extern void execute_21(char*, char *);
 IKI_DLLESPEC extern void execute_22(char*, char *);
 IKI_DLLESPEC extern void execute_23(char*, char *);
+IKI_DLLESPEC extern void execute_24(char*, char *);
+IKI_DLLESPEC extern void execute_59(char*, char *);
+IKI_DLLESPEC extern void execute_60(char*, char *);
+IKI_DLLESPEC extern void execute_61(char*, char *);
 IKI_DLLESPEC extern void execute_62(char*, char *);
 IKI_DLLESPEC extern void execute_63(char*, char *);
 IKI_DLLESPEC extern void execute_64(char*, char *);
-IKI_DLLESPEC extern void execute_65(char*, char *);
-IKI_DLLESPEC extern void execute_66(char*, char *);
-IKI_DLLESPEC extern void execute_67(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[27] = {(funcp)execute_18, (funcp)execute_56, (funcp)execute_57, (funcp)execute_58, (funcp)vlog_const_rhs_process_execute_0_fast_no_reg_no_agg, (funcp)execute_60, (funcp)execute_61, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)execute_29, (funcp)execute_25, (funcp)execute_26, (funcp)execute_20, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)execute_62, (funcp)execute_63, (funcp)execute_64, (funcp)execute_65, (funcp)execute_66, (funcp)execute_67, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 27;
+funcp funcTab[23] = {(funcp)execute_19, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)vlog_const_rhs_process_execute_0_fast_no_reg_no_agg, (funcp)execute_57, (funcp)execute_58, (funcp)execute_52, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_30, (funcp)execute_26, (funcp)execute_27, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)execute_24, (funcp)execute_59, (funcp)execute_60, (funcp)execute_61, (funcp)execute_62, (funcp)execute_63, (funcp)execute_64, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 23;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/lab4_3_tb_behav/xsim.reloc",  (void **)funcTab, 27);
+	iki_relocate(dp, "xsim.dir/lab4_3_tb_behav/xsim.reloc",  (void **)funcTab, 23);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
--- lab4/lab4/lab4.srcs/sources_1/imports/lab4/lab4_3.v
+++ lab4/lab4/lab4.srcs/sources_1/imports/lab4/lab4_3.v
@@ -14,14 +14,7 @@ module lab4_3(
     );
 
     ////////////////////////
-    wire c0, c1, c2, c3, c4, c5; // wires for intermediate carry results
-    assign c0 = in_c; // assign in_c to c0 for consistency
-    fullAdder fa0(in_a[0], ~in_b[0], c0, out_s[0], c1); // full adder for bit 0
-    fullAdder fa1(in_a[1], ~in_b[1], c1, out_s[1], c2); // full adder for bit 1
-    fullAdder fa2(in_a[2], ~in_b[2], c2, out_s[2], c3); // full adder for bit 2
-    fullAdder fa3(in_a[3], ~in_b[3], c3, out_s[3], c4); // full adder for bit 3
-    fullAdder fa4(in_a[4], ~in_b[4], c4, out_s[4], c5); // full adder for bit 4
-    assign out_c = c5; // carry output for ripple carry adder
+    lab4_2 adder(in_a, ~in_b, in_c, out_s, out_c);
     ////////////////////////
 
 endmodule
