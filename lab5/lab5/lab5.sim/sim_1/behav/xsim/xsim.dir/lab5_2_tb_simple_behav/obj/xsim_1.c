3c6204ade9a4d71caf61856ddf27ae20d9573cf3
sohnryang <loop.infinitely@gmail.com>
2023-05-10 01:19:23 +0900

    chore(lab5): vivado did something that idk

--- lab5/lab5/lab5.sim/sim_1/behav/xsim/xsim.dir/lab5_2_tb_simple_behav/obj/xsim_1.c
+++ lab5/lab5/lab5.sim/sim_1/behav/xsim/xsim.dir/lab5_2_tb_simple_behav/obj/xsim_1.c
@@ -84,19 +84,19 @@ const int NumRelocateId= 25;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/lab5_2_tb_behav/xsim.reloc",  (void **)funcTab, 25);
+	iki_relocate(dp, "xsim.dir/lab5_2_tb_simple_behav/xsim.reloc",  (void **)funcTab, 25);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
 
 void sensitize(char *dp)
 {
-	iki_sensitize(dp, "xsim.dir/lab5_2_tb_behav/xsim.reloc");
+	iki_sensitize(dp, "xsim.dir/lab5_2_tb_simple_behav/xsim.reloc");
 }
 
 void simulate(char *dp)
 {
-		iki_schedule_processes_at_time_zero(dp, "xsim.dir/lab5_2_tb_behav/xsim.reloc");
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/lab5_2_tb_simple_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
 
@@ -118,9 +118,9 @@ extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
 int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
-    iki_set_sv_type_file_path_name("xsim.dir/lab5_2_tb_behav/xsim.svtype");
-    iki_set_crvs_dump_file_path_name("xsim.dir/lab5_2_tb_behav/xsim.crvsdump");
-    void* design_handle = iki_create_design("xsim.dir/lab5_2_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+    iki_set_sv_type_file_path_name("xsim.dir/lab5_2_tb_simple_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/lab5_2_tb_simple_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/lab5_2_tb_simple_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
     return iki_simulate_design();
