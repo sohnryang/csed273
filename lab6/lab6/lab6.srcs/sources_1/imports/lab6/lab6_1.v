9ff3d38ee95a9d37f2395d1a1ae8f233db8a3ab4
sohnryang <loop.infinitely@gmail.com>
2023-05-18 16:51:06 +0900

    chore(lab6): import sample code

---
+++ lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_1.v
@@ -0,0 +1,14 @@
+/* CSED273 lab6 experiment 1 */
+/* lab6_1.v */
+
+`timescale 1ps / 1fs
+
+/* Implement synchronous BCD decade counter (0-9)
+ * You must use JK flip-flop of lab6_ff.v */
+module decade_counter(input reset_n, input clk, output [3:0] count);
+
+    ////////////////////////
+    /* Add your code here */
+    ////////////////////////
+	
+endmodule
