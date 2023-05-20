7e9267871c55432870d74bf9b5fa2c354ee3ed8b
sohnryang <loop.infinitely@gmail.com>
2023-05-21 01:44:43 +0900

    feat(lab6): implement 2 digit BCD counter

--- lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_2.v
+++ lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_2.v
@@ -8,7 +8,8 @@
 module decade_counter_2digits(input reset_n, input clk, output [7:0] count);
 
     ////////////////////////
-    /* Add your code here */
+    decade_counter dec0(reset_n, clk, count[3:0]);
+    decade_counter dec1(reset_n, count[3], count[7:4]);
     ////////////////////////
 	
 endmodule
