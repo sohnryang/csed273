cb7ca1756a1dfc4078ba346e29aba0c474607772
sohnryang <loop.infinitely@gmail.com>
2023-05-21 02:50:58 +0900

    feat(lab6): implement 369 counter

--- lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_3.v
+++ lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_3.v
@@ -9,7 +9,15 @@
 module counter_369(input reset_n, input clk, output [3:0] count);
 
     ////////////////////////
-    /* Add your code here */
+    wire d0, d1, d2, d3;
+    assign d0 = ~count[0] | ~count[2] & count[3];
+    assign d1 = ~count[2] & ~count[3] | count[2] & count[3];
+    assign d2 = count[0];
+    assign d3 = ~count[0] & count[1] | ~count[2] & count[3];
+    edge_trigger_D_FF dff0(reset_n, d0, clk, count[0]);
+    edge_trigger_D_FF dff1(reset_n, d1, clk, count[1]);
+    edge_trigger_D_FF dff2(reset_n, d2, clk, count[2]);
+    edge_trigger_D_FF dff3(reset_n, d3, clk, count[3]);
     ////////////////////////
 	
 endmodule
