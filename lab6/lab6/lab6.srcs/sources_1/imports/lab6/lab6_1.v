802346872079681146a221f5c0d0c07e5c56f699
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-21 00:55:57 +0900

    feat(lab6): add BCD counter implementation

--- lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_1.v
+++ lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_1.v
@@ -8,7 +8,12 @@
 module decade_counter(input reset_n, input clk, output [3:0] count);
 
     ////////////////////////
-    /* Add your code here */
+    wire bit0, bit0_, bit1, bit1_, bit2, bit2_, bit3, bit3_;
+    edge_trigger_JKFF jkff_bit0(reset_n, 1'b1, 1'b1, clk, bit0, bit0_);
+    edge_trigger_JKFF jkff_bit1(reset_n, bit3_, 1'b1, bit0, bit1, bit1_);
+    edge_trigger_JKFF jkff_bit2(reset_n, 1'b1, 1'b1, bit1, bit2, bit2_);
+    edge_trigger_JKFF jkff_bit3(reset_n, bit1 & bit2, 1'b1, bit0, bit3, bit3_);
+    assign count = {bit3, bit2, bit1, bit0};
     ////////////////////////
 	
 endmodule
