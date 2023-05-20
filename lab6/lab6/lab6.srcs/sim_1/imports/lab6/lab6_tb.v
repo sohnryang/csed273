0f0445bd6ebdf648e48a80bee18876bdf0d83bbe
sohnryang <loop.infinitely@gmail.com>
2023-05-21 01:45:05 +0900

    feat(lab6): add testbench for lab6_2

--- lab6/lab6/lab6.srcs/sim_1/imports/lab6/lab6_tb.v
+++ lab6/lab6/lab6.srcs/sim_1/imports/lab6/lab6_tb.v
@@ -15,16 +15,22 @@ module lab6_tb();
     wire [3:0] count;
     decade_counter counter(reset_n, clk, count);
 
+    reg [3:0] count_expected_2digits_0, count_expected_2digits_1;
+    wire [7:0] count_2digits;
+    decade_counter_2digits counter2d(reset_n, clk, count_2digits);
+
     always begin
         #5
         clk = ~clk;
     end
 
     initial begin
-        $dumpfile("wave.vcd");
-        $dumpvars();
-
         clk = 0;
+        reset_n = 0;
+        #15
+        reset_n = 1;
+
+        #195
         reset_n = 0;
         #15
         reset_n = 1;
@@ -72,7 +78,28 @@ module lab6_tb();
     /* Implement test task for lab6_2 */
     task lab6_2_test;
         ////////////////////////
-        /* Add your code here */
+        integer i;
+        
+        begin
+            $display("Testing decade_counter_2digits...");
+            count_expected_2digits_0 = 4'b0;
+            count_expected_2digits_1 = 4'b0;
+            #10
+            for (i = 0; i < 200; i = i + 1) begin
+                count_expected_2digits_0 = i % 10;
+                count_expected_2digits_1 = (i / 10) % 10;
+                #10
+                if (count_2digits[7:4] === count_expected_2digits_1 && count_2digits[3:0] === count_expected_2digits_0) begin
+                    Passed = Passed + 1;
+                end
+                else begin
+                    Failed = Failed + 1;
+                    $display("lab6_2 error: i=%d, count_expected_2digits_0=%d, count_expected_2digits_1=%d, count_2digits=%d",
+                        i, count_expected_2digits_0, count_expected_2digits_1, count_2digits);
+                end
+            end
+            $display("Testing done.");
+        end
         ////////////////////////
     endtask
 
