c05e696aee9d314e2d4f4eb4af3a349d4bde85d6
sohnryang <loop.infinitely@gmail.com>
2023-05-27 07:27:31 +0900

    docs(lab6): add comments

--- lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_1.v
+++ lab6/lab6/lab6.srcs/sources_1/imports/lab6/lab6_1.v
@@ -11,6 +11,8 @@ module decade_counter(input reset_n, input clk, output [3:0] count);
     // Bit outputs of JK flip-flops
     wire bit0, bit0_, bit1, bit1_, bit2, bit2_, bit3, bit3_;
     wire j3, k3, j2, k2, j1, k1, j0, k0;
+    
+    // Assign inputs for JK flip-flops
     assign j3 = bit0 & bit1 & bit2;
     assign k3 = bit0;
     assign j2 = bit0 & bit1;
@@ -19,6 +21,7 @@ module decade_counter(input reset_n, input clk, output [3:0] count);
     assign k1 = bit0;
     assign j0 = 1;
     assign k0 = 1;
+    
     edge_trigger_JKFF jkff_bit0(reset_n, j0, k0, clk, bit0, bit0_); // JK flip-flop for bit 0
     edge_trigger_JKFF jkff_bit1(reset_n, j1, k1, clk, bit1, bit1_); // JK flip-flop for bit 1
     edge_trigger_JKFF jkff_bit2(reset_n, j2, k2, clk, bit2, bit2_); // JK flip-flop for bit 2
